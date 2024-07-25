4d5a01395db96f661de32289c429cf44c0650457
Andrei Miroshnik <anmi.asm@gmail.com>
2024-07-25 23:58:39 +0200

    links in new tab

--- src/components/MarkdownPreview.tsx
+++ src/components/MarkdownPreview.tsx
@@ -4,6 +4,14 @@ interface MarkdownPreviewProps {
   value: string
 }
 
+const renderer = {
+  link: function (href: string, title: string, text: string) {
+    return '<a target="_blank" href="'+ href +'" title="' + title + '">' + (text ?? href) + '</a>';
+  }
+} as any
+
+marked.use({ renderer })
+
 export const MarkdownPreview = (props: MarkdownPreviewProps) => {
   return <div innerHTML={marked(props.value)}/>
 }
